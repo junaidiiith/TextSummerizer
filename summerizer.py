368e42fedec2f3baf2e8c7f66551333fe4245bd6
junaidiiith <junaid695683@gmail.com>
2018-06-19 06:08:26 +0530

    Preprocessing done

--- summerizer.py
+++ summerizer.py
@@ -11,6 +11,8 @@ from nltk import corpus
 from nltk.tokenize import RegexpTokenizer
 from nltk import stem
 import collections
+from nltk.tag import pos_tag,ne_chunk
+from nltk.tree import Tree
 
 
 stopwords = set(corpus.stopwords.words('english'))
@@ -44,20 +46,57 @@ def preprocess_para(data):
              sentences.index(sentence)) for sentence in sentences]
 
 def joinlist(l):
-    '''Flattens lists to give all the words'''
-    if type(l) == type([]):
-        x = [joinlist(i) for i in l]
-        if type(x[0]) == type("hi"):
-            return " ".join(x)
-    elif type(l[0]) == type("hi"):
-        return l
+    wordlist = list()
+    for para,index in l:
+        for sentences,ind in para:
+            wordlist += sentences
+    return wordlist
     
 def getTopNwords(paragraphs, N=10):
     word_freq = sorted(collections.Counter(joinlist(paragraphs)).most_common(),
                   key=lambda x: x[1],reverse=True)[:N]
     return [x[0] for x in word_freq]
     
+
+def number_of_thematic_words(sentence, topwords):
+    count = 0
+    for word in sentence:
+        if word in topwords:
+            count += 1
+    return count
+
+def getNumberOfProperNouns(sentence):
+    count = 0
+    for word in sentence:
+        if word[1] == 'NNP':
+            count += 1
     
+    return count
+
+def getNumberOfNumerals(sentence):
+    tokenizer = RegexpTokenizer(r'\d+.\d+')
+    return len(tokenizer.tokenize(sentence))
+
+
+def getNumberOfNamedEntities(sentence):
+     chunked = ne_chunk(pos_tag(tokenize(sentence)))
+     continuous_chunk = []
+     current_chunk = []
+     for i in chunked:
+             if type(i) == Tree:
+                     current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+             elif current_chunk:
+                     named_entity = " ".join(current_chunk)
+                     if named_entity not in continuous_chunk:
+                             continuous_chunk.append(named_entity)
+                             current_chunk = []
+             else:
+                     continue
+     return len(continuous_chunk)
+
+def create_indices(paragraphs):
+    pass
+
 def feature_extraction(paragraphs):
     
     features = ["sentence_thematic","sentence position","sentence length",
@@ -66,15 +105,22 @@ def feature_extraction(paragraphs):
                 "sentence_centroid_similarity"]
     
     topNwords = getTopNwords(paragraphs,10)
-    
-    for paragraph,index in paragraphs:
-        pass
-    
+    indices = create_indices(paragraphs)
+    sentence_feature_matrix = list()
+    sentence_pos = 0
+    for paragraph,para_index in paragraphs:
+        for sentence,sentence_pos_rel_para in paragraph:
+            sentence_pos += 1
+            sentence_thematic = number_of_thematic_words(sentence,topNwords)/len(sentence)
+            npropernouns = getNumberOfProperNouns(sentence)
+            nnumerals = getNumberOfNumerals(sentence)
+            number_of_named_entities = getNumberOfNamedEntities(sentence)
 
 def summerize(file):
     text = open(file).read().split()
-    paras = [(preprocess_para(para),text.index(para)) for para in text 
+    paras = [(preprocess_para(para,text.index(para)),text.index(para)) for para in text 
              if len(text.strip()) > 0]
+    
     feature_matrix = feature_extraction(paras)
     
     

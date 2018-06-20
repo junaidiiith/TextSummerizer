510c8d63f5ae45818183edbc0d5899d72af9bf7f
junaidiiith <junaid695683@gmail.com>
2018-06-21 03:52:32 +0530

    Completed Preprocessing

--- summerizer.py
+++ summerizer.py
@@ -8,10 +8,11 @@ Created on Tue Jun 19 02:58:20 2018
 import sys
 import nltk.data
 from nltk import corpus
-from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk import stem
 import collections
-from nltk.tag import pos_tag,ne_chunk
+from nltk.tag import pos_tag
+from nltk import ne_chunk
 from nltk.tree import Tree
 import numpy as np
 import math
@@ -20,27 +21,12 @@ import pandas as pd
 
 
 stopwords = set(corpus.stopwords.words('english'))
-
-def stemwords(words,stemmer=None):
-	if not stemmer:
-		stemmer = stem.SnowballStemmer('english')
-                                 
-	for i in range(len(words)):
-		words[i] = stemmer.stem(words[i])
-
-	return words
-
-
-def tokenize(text,tokenizer=None):
-	if not tokenizer:
-		tokenizer = RegexpTokenizer(r'\w+')
-	return tokenizer.tokenize(text)
-
+stemmer = stem.SnowballStemmer('english')
 
 def preprocess_sentence(sentence):
-    words = stemwords(tokenize(sentence))
-    sentence = [word for word in words if word not in stopwords]
-    return nltk.pos_tag(sentence)
+    sentence = word_tokenize(sentence)
+    taggedWords = nltk.pos_tag(sentence)
+    return [(stemmer.stem(word),ptag) for word,ptag in taggedWords if word.lower() not in stopwords]
     
 
 def preprocess_para(data):
@@ -79,11 +65,11 @@ def getNumberOfProperNouns(sentence):
 
 def getNumberOfNumerals(sentence):
     tokenizer = RegexpTokenizer(r'\d+.\d+')
-    return len(tokenizer.tokenize(sentence))
+    return len(tokenizer.tokenize(" ".join([word for word,_ in sentence])))
 
 
 def getNumberOfNamedEntities(sentence):
-     chunked = ne_chunk(pos_tag(tokenize(sentence)))
+     chunked = ne_chunk(pos_tag(word_tokenize(" ".join([word for word,_ in sentence]))))
      continuous_chunk = []
      current_chunk = []
      for i in chunked:
@@ -101,88 +87,110 @@ def getNumberOfNamedEntities(sentence):
 def create_indices(paragraphs):
     v = TfidfVectorizer()
     l = list()
+    
     for paragraph,paragraph_index in paragraphs:
         for sentence, sentence_index in paragraph:
-            l.append(sentence)
-    
+        	words = list()
+        	for word,tag in sentence:
+        		words.append(word)
+        	l.append(" ".join(words).strip())
     tfidf = v.fit_transform(l)
     vocab = v.vocabulary_
     return tfidf.A,vocab
 
-def get_tfisf_score(sentence,vectors,vocab):
-    val = 0.0
-    for word in sentence:
-        tf = vectors[sentence][vocab[word]]
-        isf = math.log(vectors.shape[0]/sum(vectors[:,vocab[word]]))
-        val += tf*isf
-    return val
+def get_tfisf_score(sentence,pos,vectors,vocab):
+	val = 0.0
+	for word,tag in sentence:
+		try:
+			temp = vectors[pos]
+			try:
+				
+				tf = temp[vocab[word]]
+				isf = math.log(vectors.shape[0]/sum(vectors[:,vocab[word]]))
+				val += tf*isf
+			except:
+				pass
+		except:
+			print(word.strip()," is not in the vocabulary")
+	return val
 
 def cosine_sim(s1,s2,tfisf,vocab):
-    return np.dot(tfisf[vocab[s1]], tfisf(vocab[s2]))
+	return np.dot(tfisf[s1], tfisf[s2])
 
 
 def feature_extraction(paragraphs):
     
-    features = ["sentence","sentence_thematic","sentence position",
-                "sentence length","sentence_pos_rel_para",
-                "number_of_proper_nouns",
-                "number_of_numerals","number_of_named_entities","tfisf",
-                "sentence_centroid_similarity"]
-    
-    topNwords = getTopNwords(paragraphs,10)
-    tfisf,vocab = create_indices(paragraphs)
-    sentences_feature_matrix = list()
-    sentence_pos = 0
-    scores = list()
-    for paragraph,para_index in paragraphs:
-        for sentence,sentence_pos_rel_para in paragraph:
-            sentence_pos += 1
-            sentence_thematic = number_of_thematic_words(sentence,topNwords)/len(sentence)
-            npropernouns = getNumberOfProperNouns(sentence)
-            nnumerals = getNumberOfNumerals(sentence)
-            number_of_named_entities = getNumberOfNamedEntities(sentence)
-            tfisf = get_tfisf_score(sentence,tfisf,vocab)
-            scores.append((tfisf,sentence))
-            sentence_length = sum(len(word) for word in sentence)
-            sentences_feature_matrix = [sentence,sentence_pos,sentence_thematic, 
-                                   sentence_length, sentence_pos_rel_para,
-                                   npropernouns, nnumerals,
-                                   number_of_named_entities,tfisf]
-            
-    _, centroid_sentence = max(scores)
-    sentences_feature_matrix_ = list()
-    for sentence_attributes in sentences_feature_matrix:
-        l = sentence_attributes
-        l.append(cosine_sim(centroid_sentence,
-                            sentence_attributes[0],tfisf,vocab))
-        sentences_feature_matrix_.append(l)
-    
-    sentences_feature_matrix = None
-    return pd.DataFrame(np.array(sentences_feature_matrix_),columns=features)
+	features = ["sentence","sentence position","sentence_thematic",
+		"sentence length","sentence_pos_rel_para",
+		"number_of_proper_nouns",
+		"number_of_numerals","number_of_named_entities","tfisf",
+		"sentence_centroid_similarity"]
+	topNwords = getTopNwords(paragraphs,10)
+	tfisf,vocab = create_indices(paragraphs)
+	sentences_feature_matrix = list()
+	sentence_pos = 0
+	scores = list()
+	for paragraph,para_index in paragraphs:
+		for sentence,sentence_pos_rel_para in paragraph:
+			# print("sentence is", sentence)
+			sentence_thematic = number_of_thematic_words(sentence,topNwords)/len(sentence)
+			# print("Sentence thematic ",sentence_thematic)
+			npropernouns = getNumberOfProperNouns(sentence)
+			# print("Number of proper nouns ",npropernouns)
+			nnumerals = getNumberOfNumerals(sentence)
+			# print("number of numerals ",nnumerals)
+			number_of_named_entities = getNumberOfNamedEntities(sentence)
+			# print("number of named entities ",number_of_named_entities)
+			val = get_tfisf_score(sentence,sentence_pos,tfisf,vocab)
+			# print("tsisf score ",val)
+			scores.append((val,sentence_pos))
+			sentence_length = len(sentence)
+			# print(sentence_length)
+			sentences_feature_matrix.append([sentence,sentence_pos,sentence_thematic, 
+			               sentence_length, sentence_pos_rel_para,
+			               npropernouns, nnumerals,
+			               number_of_named_entities,val])
+			# x = input("Enter to continue")
+			sentence_pos += 1
+
+	_, centroid_sentence_index = max(scores)
+	sentences_feature_matrix_ = list()
+	for sentence_attributes in sentences_feature_matrix:
+		l = sentence_attributes
+		l.append(cosine_sim(centroid_sentence_index,
+		            sentence_attributes[1],tfisf,vocab))
+		sentences_feature_matrix_.append(l)
+
+	sentences_feature_matrix = None
+	return pd.DataFrame(np.array(sentences_feature_matrix_),columns=features)
 
 
 def train_and_plot_results(data):
     pass     
 
 def summerize(file):
-    text = open(file).read().split()
-    paras = [(preprocess_para(para,text.index(para)),text.index(para)) for para in text 
-             if len(text.strip()) > 0]
+	text = open(file).read().split('\n\n')
+	paras = [para.replace('\n',' ') for para in text]
+	paras = [(preprocess_para(para),paras.index(para)) for para in paras 
+	         if len(para) > 0]
     
-    data = feature_extraction(paras)
-    train_and_plot_results(data)
-    
-
-def main():
-    if sys.argc < 1:
-        print("Please enter the file to summerize")
-        sys.exit(0)
-    
-    files = sys.argv[1:]
-    for file in files:
-        print(summerize(file))
+	data = feature_extraction(paras)
+	print(data.head())
+	print(data.columns)
+	train_and_plot_results(data)
     
 
-if __name__ == '__main__':
-	main()
+summerize('article8')
+# def main():
+#     if sys.argc < 1:
+#         print("Please enter the file to summerize")
+#         sys.exit(0)
+    
+#     files = sys.argv[1:]
+#     for file in files:
+#         print(summerize(file))
+    
+
+# if __name__ == '__main__':
+# 	main()
     
